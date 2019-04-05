348d15ae03f67a9b8428d542ce1c31d1d01c1582
albert <albert.kulinski@hotmail.com>
2019-04-05 19:37:28 +0200

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/com/flatter/messagingservice/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package com.flatter.messagingservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Messagingservice.
+ * <p>
+ * Properties are configured in the application.yml file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
---
+++ src/main/java/com/flatter/messagingservice/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Service layer beans.
+ */
+package com.flatter.messagingservice.service;
